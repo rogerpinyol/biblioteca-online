9abafde63fcb473de220b34211b321f7b8cab5a7
sergiobiol <sergiobiol@iesmontsia.org>
2025-03-14 14:27:30 +0100

    Canvis sergi classe

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'  # Replace with a secure random key
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/llibre')
+def llibre():
+    return render_template('llibre.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5500)  # Executa l'aplicació al port 5500
