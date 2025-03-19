a2b58cd38189818dbefd1b69323db720787b6d2d
Roger <rogerpinol@iesmontsia.org>
2025-03-19 14:16:24 +0100

    a a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'  # Canvia això per una clau segura
